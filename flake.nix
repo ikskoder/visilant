2a59da4ecc8be3da1547334a0bd8dc016427424b
X Coder <ikskoder@gmail.com>
2025-11-20 22:33:02 +0200

    Add flake.nix and update .gitignore to include environment files

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Visilant browser extension development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            pnpm
+            just
+          ];
+
+          shellHook = ''
+            echo "Welcome to the Visilant development environment!"
+            echo "Run 'pnpm install' to install dependencies."
+            echo "Run 'just --list' to see all available commands."
+
+          '';
+        };
+      }
+    );
+}
